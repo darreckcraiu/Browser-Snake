478aecc51f5eed7c1da9412c099158fb7defc478
Darreck <153337041+Darreck1@users.noreply.github.com>
2025-06-11 14:51:21 -0700

    modified config to use locally stored values for the number of rows and cols on the grid. These values can also be changed through searchbar parameters

--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,11 @@
-export const rows = 15;
-export const cols = 15;
+//use the parameters saved on local storage if they exist
+const savedParams = localStorage.getItem("savedParams");
+const params = new URLSearchParams(
+  savedParams !== null ? savedParams : window.location.search
+)
+
+export const rows = getValidNumber("rows", 15);
+export const cols = getValidNumber("cols", 15);
 export const snakeArrSize = 500;
 export const snakeColor = 'rgb(145, 255, 123)';
 export const foodColor = 'rgb(255, 123, 123)';
@@ -13,3 +19,16 @@ export const cellStyles = {
 }
 export const gridBorderColor = 'rgb(255, 255, 255)';
 export const gameloopInterval = 100;
+
+//returns a parameter if it is a number, else returns fallback
+function getValidNumber(paramName, fallback) {
+  const value = params.get(paramName);
+  const num = Number(value);
+  return value !== null && !isNaN(num) ? num : fallback;
+}
+
+if (rows != 15 || cols != 15) {
+  localStorage.setItem(
+    "savedParams", 
+    `?rows=${rows}&cols=${cols}`);
+}
